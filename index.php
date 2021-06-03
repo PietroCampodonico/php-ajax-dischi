7cd4d9100ca44c1bdf65e0e08385218223b758d6
PietroCampodonico <pietro.campodonico@outlook.it>
2021-06-03 19:50:57 +0200

    Milestone 1 completata

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
-<?php 
+<?php
 include "music_albums.php"
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="dist/app.css">
-    <title>My Music Albums</title>
+
+    <title>MyJam</title>
 </head>
+
 <body>
-    
+    <header>
+        <h1>MyJam</h1>
+    </header>
+
+    <main>
+        <div class="album-container">
+            <?php
+            foreach ($music_albums as $album) {
+            ?>
+                <div class="album">
+                    <img src="<?php echo $album["poster"]; ?>" alt="">
+                    <h3 class="album-title"><?php echo $album["author"]; ?></h3>
+                    <h4 class="album-release-year"><?php echo $album["year"]; ?></h4>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </main>
+
 </body>
+
 </html>
